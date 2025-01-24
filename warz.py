c098fca7a0112d99f43757f002caf7cf1ebb3540
guitarheror <guilhermecarreiroclaudino@gmail.com>
2025-01-24 01:01:24 -0300

    Add files via upload

---
+++ warz.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+root = Tk()
+
+root.mainloop()
+j1 = 0
+j2 = 0
+j3 = 0
+j4 = 0
+j5 = 0
+j6 = 0
+
+njogadores = int(input("Quantas pessoas vão jogar?"))
+if njogadores>0 and njogadores<7:
+    print("QB")
